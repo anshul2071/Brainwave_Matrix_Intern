2a026945e5c738b676fc70e3d201dca685ecfb47
Anshul Rawal <104826906+anshul2071@users.noreply.github.com>
2024-11-18 20:53:19 +0545

    third

--- main.js
+++ main.js
@@ -16,3 +16,43 @@ menuBTN.addEventListener("click", (e) =>{
         navLinks.classList.remove("open");
         menuBTNIcon.setAttribute("class", "ri-menu-line");
         });
+
+
+const scrollRevealOption = {
+            distance: "60px",
+            origin: "bottom",
+            duration: 1000,
+            
+        };
+ScrollReveal().reveal(".header-image",{
+    ...scrollRevealOption,
+    origin: "right",
+
+} );
+
+ScrollReveal().reveal(".header-content h1",{
+    ...scrollRevealOption,
+    delay: 500,
+} );
+ScrollReveal().reveal(".header-content p",{
+    ...scrollRevealOption,
+    delay:1000,
+} );
+ScrollReveal().reveal(".header-content form",{
+    ...scrollRevealOption,
+    delay:1500,
+
+} );
+
+ScrollReveal().reveal(".header-content bar",{
+    ...scrollRevealOption,
+    delay:2500,
+
+} );
+
+ScrollReveal().reveal(".header-image-card",{
+    duration: 1000,
+    interval: 500,
+    delay: 500,
+
+} );
